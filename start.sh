1babdc1997bd85010d9d15103f86ecdab2b3f273
winnershuai <51901340+winnershuai@users.noreply.github.com>
2022-06-14 21:43:54 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ yum update
 yum install ntp -y
 ntpdate ntp.aliyun.com
 hwclock -w
-yum install bash-completion
+yum install -y bash-completion
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum makecache fast
