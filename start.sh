7f23ef6a7e990ec52350dd8dc6676224c702bfb5
winnershuai <51901340+winnershuai@users.noreply.github.com>
2022-06-23 00:45:14 +0800

    Create start.sh

--- start.sh
+++ start.sh
@@ -33,7 +33,7 @@ br_netfilter
 EOF
 cat << EOF |sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
 EOF
 sysctl --system
 
